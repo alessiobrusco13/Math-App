09107d79e5d3404c68af66c83fc155ffe15bc2c7
Alessio Garzia Marotta Brusco <alessiobrusco609@gmail.com>
2022-04-30 23:00:09 +0200

    Added math to calculator.

---
+++ Math-Project/Components/FractionView.swift
@@ -0,0 +1,20 @@
+//
+//  FractionView.swift
+//  Math-Project
+//
+//  Created by Alessio Garzia Marotta Brusco on 30/04/22.
+//
+
+import SwiftUI
+
+struct FractionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FractionView_Previews: PreviewProvider {
+    static var previews: some View {
+        FractionView()
+    }
+}
---
+++ Math-Project/Extensions/OperatorSet-default.swift
@@ -0,0 +1,8 @@
+//
+//  OperatorSet-default.swift
+//  Math-Project
+//
+//  Created by Alessio Garzia Marotta Brusco on 30/04/22.
+//
+
+import Foundation
