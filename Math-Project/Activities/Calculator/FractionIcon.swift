a79090262d5d0818adb999ec3ede1f0e024f5dce
Alessio Garzia Marotta Brusco <alessiobrusco609@gmail.com>
2022-05-01 12:07:37 +0200

    Added fraction button to calculator.

---
+++ Math-Project/Activities/Calculator/FractionIcon.swift
@@ -0,0 +1,20 @@
+//
+//  FractionIcon.swift
+//  Math-Project
+//
+//  Created by Alessio Garzia Marotta Brusco on 01/05/22.
+//
+
+import SwiftUI
+
+struct FractionIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FractionIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        FractionIcon()
+    }
+}
